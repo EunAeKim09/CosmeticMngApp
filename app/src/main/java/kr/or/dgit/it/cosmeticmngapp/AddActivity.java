5b9288944daea525b74923dd46c365507aa8c3a7
EunAeKim <eunae0474@naver.com>
2018-05-30 09:35:04 +0900

    add layout setting

---
+++ app/src/main/java/kr/or/dgit/it/cosmeticmngapp/AddActivity.java
@@ -0,0 +1,121 @@
+package kr.or.dgit.it.cosmeticmngapp;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AddActivity extends AppCompatActivity {
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int CROP_FROM_IMAGE = 2;
+    private String mCurrentPhotoPath;
+    private Uri photoUri = null;
+    private Uri albumUri = null;
+    private boolean album;
+
+    private CharSequence[] info;
+
+    private ImageView imgview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_add_cosmetic);
+
+
+
+        imgview = (ImageView)findViewById(R.id.img);
+    }
+
+    public void categoryBtnClick(View view) {
+        CharSequence info[] = new CharSequence[]{"내정보", "로그아웃"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setItems(info, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which)
+                {
+                    case 0:
+                        Toast.makeText(AddActivity.this, "내정보", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(AddActivity.this, "로그아웃", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, PICK_FROM_CAMERA);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",  /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    public void cameraClick(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, PICK_FROM_CAMERA);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_FROM_CAMERA && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgview.setImageBitmap(imageBitmap);
+        }
+    }
+}
--- app/src/main/java/kr/or/dgit/it/cosmeticmngapp/MainActivity.java
+++ app/src/main/java/kr/or/dgit/it/cosmeticmngapp/MainActivity.java
@@ -1,75 +1,114 @@
 package kr.or.dgit.it.cosmeticmngapp;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    DrawerLayout drawerLayout;
+public class MainActivity extends android.support.v7.app.AppCompatActivity implements android.support.design.widget.NavigationView.OnNavigationItemSelectedListener{
+    android.support.v4.widget.DrawerLayout drawerLayout;
+    private static String[] PERMISSIONS = {android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE};
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(android.os.Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ActionBar actionBar = getSupportActionBar();
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true);
-        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle("화장품 관리");
         actionBar.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
+
+        toolbar.setNavigationIcon(R.drawable.menu);
 
         drawerLayout = findViewById(R.id.drawerlayout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.openT,R.string.closeT);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = findViewById(R.id.navigation);
+        android.support.design.widget.NavigationView navigationView = findViewById(R.id.navigation);
         navigationView.setNavigationItemSelectedListener(this);
 
-
+        //permission
+       /* if(!checkPermission(this, Manifest.permission.CAMERA)||!checkPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                ||!checkPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+            requestExternalPermissions(this);
+        }*/
     }
 
+    /*public static int checkSelfPermission(@NonNull android.content.Context context, @NonNull String permission){ //권한 체크
+        if(permission == null){
+            throw new IllegalArgumentException("permission is null");
+        }
+        return context.checkPermission(permission, android.os.Process.myPid(), Process.myUid());
+    }
+
+    public static boolean checkPermission(Activity activity, String permission){
+        int permissionResult = ActivityCompat.checkSelfPermission(activity, permission);
+
+        if(permissionResult == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void requestPermissions(final  @NonNull Activity activity, final @NonNull String[] permissions, final int requestCode){
+        if(Build.VERSION.SDK_INT >= 23){
+            ActivityCompat.requestPermissions(activity, permissions, requestCode);
+        }else if(activity instanceof ActivityCompat.OnRequestPermissionsResultCallback){
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(()->{
+                final int[] grantResults = new int[permissions.length];
+                PackageManager packageManager = activity.getPackageManager();
+                String packageName = activity.getPackageName();
+
+                final int permissionCount = permissions.length;
+                for(int i=0; i<permissionCount; i++){
+                    grantResults[i] = packageManager.checkPermission(permissions[i], packageName);
+                }
+                ((ActivityCompat.OnRequestPermissionsResultCallback)activity).onRequestPermissionsResult(requestCode, permissions, grantResults);
+            });
+        }
+    }
+
+    public static void requestExternalPermissions(Activity activity){
+        ActivityCompat.requestPermissions(activity, PERMISSIONS,1);
+    }
+*/
+
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    public boolean onNavigationItemSelected(@android.support.annotation.NonNull android.view.MenuItem item) {
         int id=item.getItemId();
         if(id==R.id.app_menu1){
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
             fragmentTransaction.add(R.id.fragment_container, MyItemList.newInstance());
             fragmentTransaction.commit();
         }else if(id==R.id.app_menu2){
-
+            android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.fragment_container, MyItemList.newInstance());
+            fragmentTransaction.commit();
         }else if(id==R.id.app_menu3){
-
+            android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.fragment_container, MyItemList.newInstance());
+            fragmentTransaction.commit();
         }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerlayout);
-        drawer.closeDrawer(GravityCompat.START);
+        android.support.v4.widget.DrawerLayout drawer = (android.support.v4.widget.DrawerLayout) findViewById(R.id.drawerlayout);
+        drawer.closeDrawer(android.support.v4.view.GravityCompat.START);
         return true;
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        android.view.MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_set, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -77,16 +116,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         switch (id) {
             case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
+                drawerLayout.openDrawer(android.support.v4.view.GravityCompat.START);
                 return true;
             case R.id.searchIcon:
-                drawerLayout.openDrawer(GravityCompat.START);
+                drawerLayout.openDrawer(android.support.v4.view.GravityCompat.START);
                 return true;
             case R.id.registerIcon:
-                drawerLayout.openDrawer(GravityCompat.START);
+                android.content.Intent intent = new android.content.Intent(this, AddActivity.class);
+                startActivity(intent);
                 return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
